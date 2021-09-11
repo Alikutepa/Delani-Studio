835a803db0edc51dbe4e105d34a25cf282748ec5
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-09-11 19:27:23 +0300

    add toggle function to design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  
-})
+  $(".design").click(function(){
+      $(".img1").toggle();
+      $(".text1").toggle();
+  });
+  $(".development").click(function(){
+      $(".wedo2").toggle();
+      $(".info2").toggle();
+  });
+  $(".product").click(function(){
+      $(".wedo3").toggle();
+      $(".info3").toggle();
+  });
+});
