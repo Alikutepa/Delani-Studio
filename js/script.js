cd982db2bf0ae519ff7b61d7057b99c82e78746a
Alikutepa <juliana.alikutepa@student.moringaschool.com>
2021-09-12 21:17:30 +0300

    add changes

--- js/script.js
+++ js/script.js
@@ -25,88 +25,88 @@ $(document).ready(function(){
    
   
 //});
-
-  $('#image1 a img').hover(function () {
+$(document).ready( function() {
+    $('#image1 a img').hover(function () {
+      $(this).stop().animate({
+          opacity: .3
+      }, 200);
+      $('.text1').removeClass('hide');
+  }, function () {
+      $(this).stop().animate({
+          opacity: 1}, 600);
+      $('.text1').addClass('hide');
+  });
+  $('#image2 a img').hover(function () {
     $(this).stop().animate({
         opacity: .3
     }, 200);
-    $('.text1').removeClass('hide');
-}, function () {
+    $('.text2').removeClass('hide');
+  }, function () {
     $(this).stop().animate({
         opacity: 1}, 600);
-    $('.text1').addClass('hide');
+    $('.text2').addClass('hide');
+  });
+  $('#image3 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text3').removeClass('hide');
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text3').addClass('hide');
+  });
+  $('#image4 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text4').removeClass('hide');
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text4').addClass('hide');
+  });
+  $('#image5 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text5').removeClass('hide');
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text5').addClass('hide');
+  });
+  $('#image6 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text6').removeClass('hide');
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text6').addClass('hide');
+  });
+  $('#image7 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text7').removeClass('hide');
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text7').addClass('hide');
+  });
+  $('#image8 a img').hover(function () {
+    $(this).stop().animate({
+        opacity: .3
+    }, 200);
+    $('.text8').removeClass('hide');
+  }, function () {
+    $(this).stop().animate({
+        opacity: 1}, 600);
+    $('.text8').addClass('hide');
+  });
 });
-$('#image2 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text2').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text2').addClass('hide');
-});
-$('#image3 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text3').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text3').addClass('hide');
-});
-$('#image4 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text4').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text4').addClass('hide');
-});
-$('#image5 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text5').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text5').addClass('hide');
-});
-$('#image6 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text6').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text6').addClass('hide');
-});
-$('#image7 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text7').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text7').addClass('hide');
-});
-$('#image8 a img').hover(function () {
-  $(this).stop().animate({
-      opacity: .3
-  }, 200);
-  $('.text8').removeClass('hide');
-}, function () {
-  $(this).stop().animate({
-      opacity: 1}, 600);
-  $('.text8').addClass('hide');
-});
-
 
 //form validation and thank you message
 function popUpMessage(){
